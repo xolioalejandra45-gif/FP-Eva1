609bf748f2e726179d9d527e729e75ac39b74957
Alejandra Xolio <xolioalejandra45@gmail.com>
2025-08-27 23:37:13 -0600

    Eva1_4_Ejercicio_25550473

---
+++ Eva1_4_Ejercicio/src/eva1_4_ejercicio/Eva1_4_Ejercicio.java
@@ -0,0 +1,16 @@
+package eva1_4_ejercicio;
+
+public class Eva1_4_Ejercicio {
+
+    public static void main(String[] args) {
+        System.out.print("Empleo: ");
+        System.out.println("Médico");
+        System.out.print("Salario: ");
+        System.out.println(8740);
+        System.out.print("Horas a la semana: ");
+        System.out.println("38");
+        System.out.print("Requiere inglés? ");
+        System.out.println(true);
+    }
+    
+}
