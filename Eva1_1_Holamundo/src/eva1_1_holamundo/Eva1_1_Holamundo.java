21c60b0a471eabaa12ac7f36b6fd9fab42f8a0b4
Alejandra Xolio <xolioalejandra45@gmail.com>
2025-08-27 23:33:09 -0600

    Eva1_1_Hola_Mundo_25550473

---
+++ Eva1_1_Holamundo/src/eva1_1_holamundo/Eva1_1_Holamundo.java
@@ -0,0 +1,8 @@
+package eva1_1_holamundo;
+
+public class Eva1_1_Holamundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Fundamentos de programación");
+    }
+}
