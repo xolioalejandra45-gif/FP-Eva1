7e93dc003eb135d56cfc082ac340d9512bf30deb
Alejandra Xolio <xolioalejandra45@gmail.com>
2025-08-27 23:34:23 -0600

    Eva1_2_Datos_25550473

---
+++ Eva1_2_Datos/src/eva1_2_datos/Eva1_2_Datos.java
@@ -0,0 +1,19 @@
+package eva1_2_datos;
+public class Eva1_2_Datos {
+
+    public static void main(String[] args) {
+        System.out.println("Alejandra Lezama"); 
+        //; significa fin de instrucción
+        // números enteros
+        System.out.println(1010);
+        // números decimales
+        System.out.println(5.7);
+        // valores lógicos (verdadero o falso)
+        // true false
+        System.out.println(true);
+        // caracteres individuales
+        // cosas distintas
+        System.out.println('c'); //caracter
+        System.out.println("c"); //cadena de texto
+   } 
+}
