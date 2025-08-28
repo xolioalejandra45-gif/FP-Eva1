7fc9d8c7c786b67644b09ae065184d9c8567224a
Alejandra Xolio <xolioalejandra45@gmail.com>
2025-08-27 23:36:10 -0600

    Eva1_3_Datos_25550473

---
+++ Eva1_3_Datos/src/eva1_3_datos/Eva1_3_Datos.java
@@ -0,0 +1,14 @@
+package eva1_3_datos;
+
+public class Eva1_3_Datos {
+
+    public static void main(String[] args) {
+        System.out.print("Nombre del estudiante: ");
+        System.out.println("Alejandra Lezama Xolio");
+        System.out.print("Número de control: ");
+        System.out.println(25550473);
+        System.out.print("Edad: ");
+        System.out.println(17);
+    }
+    
+}
