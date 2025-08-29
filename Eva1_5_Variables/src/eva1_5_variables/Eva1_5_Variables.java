fe78d86a844dbe4f5740b8bfb923046356ecee71
Alejandra Xolio <xolioalejandra45@gmail.com>
2025-08-28 23:56:17 -0600

    Eva1_5_Variables_25550473

---
+++ Eva1_5_Variables/src/eva1_5_variables/Eva1_5_Variables.java
@@ -0,0 +1,18 @@
+package eva1_5_variables;
+
+public class Eva1_5_Variables {
+
+    public static void main(String[] args) {
+        String nombre;
+        int edad;
+        double salario;
+        
+        nombre = "Alejandra Lezama Xolio";
+        edad = 17;
+        salario = 200.6;
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+    }
+    
+}
