1c1c78f0968a5380cc453c5928c594827bd5d3d8
Alejandra Xolio <xolioalejandra45@gmail.com>
2025-09-01 16:48:46 -0600

    Eva1_6_operaciones_25550473

---
+++ Eva1_6_operaciones/src/eva1_6_operaciones/Eva1_6_operaciones.java
@@ -0,0 +1,15 @@
+package eva1_6_operaciones;
+
+public class Eva1_6_operaciones {
+
+    public static void main(String[] args) {
+        double area, pi, radio;
+        
+        pi= 3.1416;
+        radio = 10;
+        area = pi * radio * radio;
+        System.out.println("El área de un círculo de radio = 10 es ");
+        System.out.println(area);
+    }
+    
+}
