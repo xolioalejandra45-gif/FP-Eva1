2b0fcc6637556914146716b100fbadaef19caf2b
Alejandra Xolio <xolioalejandra45@gmail.com>
2025-09-01 22:36:21 -0600

    Eva1_8_conversion_25550473

---
+++ Eva1_8_conversion_fahrenheit_a_celsius/src/eva1_8_conversion_fahrenheit_a_celsius/Eva1_8_conversion_fahrenheit_a_celsius.java
@@ -0,0 +1,15 @@
+package eva1_8_conversion_fahrenheit_a_celsius;
+
+public class Eva1_8_conversion_fahrenheit_a_celsius {
+
+    public static void main(String[] args) {
+        double F, C;
+        
+        F = 100;
+        C = (5*(F-32))/9; 
+        
+        System.out.println("La conversión de 100°F es ");
+        System.out.println(C);
+    }
+    
+}
