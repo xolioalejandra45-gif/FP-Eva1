4971ead9073904349c612b2e8b279fcd89d5e657
Alejandra Xolio <xolioalejandra45@gmail.com>
2025-09-01 22:35:05 -0600

    Eva1_7_area_triangulo_25550473

---
+++ Eva1_7_area_triangulo/src/eva1_7_area_triangulo/Eva1_7_area_triangulo.java
@@ -0,0 +1,16 @@
+package eva1_7_area_triangulo;
+
+public class Eva1_7_area_triangulo {
+
+    public static void main(String[] args) {
+       double area, base, altura;
+       
+       base= 10.3;
+       altura = 15.5;
+       area = (base * altura) / 2;
+       
+       System.out.println("El área de un triangulo con base = 10.3, altura = 15.5 es ");
+       System.out.println(area);
+    }
+    
+}
